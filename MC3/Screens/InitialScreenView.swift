5083daa0a17c30ac33888132256b77c5b2b249cf
LeoLodra <leonardomarhan5@gmail.com>
2024-08-13 17:46:20 +0700

    chore

---
+++ MC3/Screens/InitialScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  InitialScreenView.swift
+//  MC3
+//
+//  Created by Leonardo Marhan on 13/08/24.
+//
+
+import UIKit
+
+class InitialScreenView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+    }
+}
+
+#Preview {
+    InitialScreenView()
+}
