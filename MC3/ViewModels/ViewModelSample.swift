da9b0cc763b14aead08fecdb2716ee84d2b06091
LeoLodra <leonardomarhan5@gmail.com>
2024-08-13 17:53:10 +0700

    chore: fix group issue

---
+++ MC3/ViewModels/ViewModelSample.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModelSample.swift
+//  MC3
+//
+//  Created by Leonardo Marhan on 13/08/24.
+//
+
+import Foundation
---
+++ MC3/Views/ViewSample.swift
@@ -0,0 +1,8 @@
+//
+//  ViewSample.swift
+//  MC3
+//
+//  Created by Leonardo Marhan on 13/08/24.
+//
+
+import Foundation
