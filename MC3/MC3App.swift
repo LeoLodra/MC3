45e5a9e232270d0ac14fb343dd06fe3edf881290
Danzel Artamadja <notwatermango@gmail.com>
2024-08-23 03:43:58 +0700

    Fix preventing conflict on MC3App.swift from useless change

--- MC3/MC3App.swift
+++ MC3/MC3App.swift
@@ -43,7 +43,7 @@ struct MC3App: App {
 
     var body: some Scene {
         WindowGroup {
-            NutrientDetailView()
+            InitialViewControllerRepresentable()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
